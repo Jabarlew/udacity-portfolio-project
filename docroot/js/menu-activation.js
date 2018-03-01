c31dfd86009441e7c20db1191167863c5e94a65c
Bartosz Lewandowski <barlewdesign@gmail.com>
2018-03-01 18:15:13 +0100

    working page

--- docroot/js/menu-activation.js
+++ docroot/js/menu-activation.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
+
   const menuAnimation1 = document.querySelector('.animation-circle');
   const menuAnimation4 = document.querySelector('.menu-fixed-list');
-  const navColor = document.querySelector('.fa-bars');
+  const navIconChange = document.querySelector('.fa-bars');
   const navToggleElement = document.querySelector('.main-header-nav-toggle');
+  const menuToggleOffClick = document.querySelectorAll(".menu-toggle");
 
-
-  navToggleElement.addEventListener('click', function () {
+  function menuOpenClose() {
     menuAnimation1.classList.toggle('animation-circle-off');
     menuAnimation4.classList.toggle('menu-fixed-list-off');
-    navColor.classList.toggle('fa-bars-off');
+    navIconChange.classList.toggle('fa-bars-off',);
+    navIconChange.classList.toggle('fa-times');
+    navIconChange.classList.toggle('fa-bars');
+  }
+
+  navToggleElement.addEventListener('click', menuOpenClose);
+  menuToggleOffClick.forEach(function (element) {
+    element.addEventListener('click', menuOpenClose);
   });
 });
